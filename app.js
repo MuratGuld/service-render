11efe38a8b26d69426eb0b9aa20648445aee4eac
MuratGuld <slimplus1952@gmail.com>
2022-12-14 01:36:03 +0100

    deneme

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const data = [
+  { id: 1, name: "murat", age: 32 },
+  { id: 2, name: "ahmet", age: 32 },
+];
+
+app.get("/", function (req, res) {
+  res.send(data);
+});
+
+app.get("/:id", function (req, res) {
+  let id = req.params.id;
+  let dataById = data.find((object) => object.id == id);
+  res.send(dataById);
+});
+
+app.listen(3000);
